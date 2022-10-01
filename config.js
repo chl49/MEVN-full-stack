a6534d9f2d4554c66d0ffad5487dde4d698549ba
chl49 <chl49@sfu.ca>
2022-09-30 17:31:13 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoUri: process.env.MONGO_URI,
+    //mongoUri: 'mongodb+srv://chl49:VegaAltair77@clustermongo.jid8q7i.mongodb.net/?retryWrites=true&w=majority',
+    PORT: process.env.PORT || 3000,
+}
+
+//export MONGO_URI='mongodb
